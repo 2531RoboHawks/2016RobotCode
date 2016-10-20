baeea82f8359309a819b9035d6dd135085db824b
Chase <chasereinertson@gmail.com>
2016-10-20 10:12:02 -0500

    updated the frclib packages

--- Bert/src/frclib/vision/Camera.java
+++ Bert/src/frclib/vision/Camera.java
@@ -3,7 +3,6 @@ package frclib.vision;
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
 
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.vision.AxisCamera;
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
@@ -11,6 +10,7 @@ public class Camera {
 
 	USBCamera cam;
 	AxisCamera axiscam;
+	String name = "cam0";
 
 	public Camera(USBCamera cam) {
 		this.cam = cam;
@@ -20,34 +20,16 @@ public class Camera {
 		this.axiscam = cam;
 	}
 
-	public void sendLiveFeed() {
-		if (cam != null || axiscam != null) {
-			Image image = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-			getImage(image);
-			CameraServer.getInstance().setImage(image);
-		} else {
-			return;
-		}
+	public Camera(String cam) {
+		this.name = cam;
 	}
 
-	public void setFPS(int fps) {
-		if (cam != null) {
-			cam.setFPS(fps);
-		} else if (axiscam != null) {
-			axiscam.writeMaxFPS(fps);
-		} else {
-			return;
-		}
+	public String getName() {
+		return name;
 	}
 
-	public void getImage(Image image) {
-		if (cam != null) {
-			cam.getImage(image);
-		} else if (axiscam != null) {
-			axiscam.getImage(image);
-		} else {
-			return;
-		}
+	public void getImage(Image image, int session) {
+		NIVision.IMAQdxGrab(session, image, 1);
 	}
 
 }
--- Bert/src/frclib/vision/VisionTracking.java
+++ Bert/src/frclib/vision/VisionTracking.java
@@ -18,11 +18,44 @@ public class VisionTracking {
 	NIVision.Range green = new NIVision.Range();
 	NIVision.Range blue = new NIVision.Range();
 
+	boolean tracking = false;
+
+	boolean captureing = false;
+
+	int session;
+
+	public boolean isStarted = false;
+
 	public VisionTracking(Camera cam) {
 		this.cam = cam;
 		frame = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
 		filteredframe = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
 		displayframe = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+		session = NIVision.IMAQdxOpenCamera(cam.getName(),
+				NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+		NIVision.IMAQdxConfigureGrab(session);
+	}
+
+	public void startCam() {
+		tracking = true;
+		captureing = true;
+		NIVision.IMAQdxStartAcquisition(session);
+		isStarted = true;
+	}
+
+	public void stopCam() {
+		NIVision.IMAQdxStopAcquisition(session);
+		isStarted = false;
+		tracking = false;
+		captureing = false;
+	}
+
+	public void displayRawImage() {
+		if (captureing) {
+			cam.getImage(frame, session);
+			CameraServer.getInstance().setImage(frame);
+
+		}
 	}
 
 	public void setColor(int rmax, int rmin, int gmax, int gmin, int bmax, int bmin) {
@@ -35,30 +68,36 @@ public class VisionTracking {
 	}
 
 	public void displayBlobs() {
-		cam.getImage(frame);
-		ArrayList<NIVision.Rect> blobs = getBlobs();
-		for (NIVision.Rect blob : blobs) {
-			NIVision.imaqCopyRect(displayframe, frame, blob, new NIVision.Point(blob.left, blob.top));
+		if (tracking) {
+			cam.getImage(frame, session);
+			ArrayList<NIVision.Rect> blobs = getBlobs();
+			for (NIVision.Rect blob : blobs) {
+				NIVision.imaqCopyRect(displayframe, frame, blob, new NIVision.Point(blob.left, blob.top));
+			}
+			CameraServer.getInstance().setImage(displayframe);
 		}
-		CameraServer.getInstance().setImage(displayframe);
 	}
 
 	public ArrayList<NIVision.Rect> getBlobs() {
-		cam.getImage(frame);
-		ArrayList<NIVision.Rect> blobs = new ArrayList<NIVision.Rect>();
-		NIVision.imaqColorThreshold(filteredframe, frame, 255, NIVision.ColorMode.RGB, red, green, blue);
-		int numblobs = NIVision.imaqCountParticles(filteredframe, 1);
-		for (int i = 0; i < numblobs; i++) {
-			int top = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
-					NIVision.MeasurementType.MT_BOUNDING_RECT_TOP);
-			int left = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
-					NIVision.MeasurementType.MT_BOUNDING_RECT_LEFT);
-			int width = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
-					NIVision.MeasurementType.MT_BOUNDING_RECT_WIDTH);
-			int height = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
-					NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT);
-			blobs.add(new NIVision.Rect(top, left, height, width));
+		if (tracking) {
+			cam.getImage(frame, session);
+			ArrayList<NIVision.Rect> blobs = new ArrayList<NIVision.Rect>();
+			NIVision.imaqColorThreshold(filteredframe, frame, 255, NIVision.ColorMode.RGB, red, green, blue);
+			int numblobs = NIVision.imaqCountParticles(filteredframe, 1);
+			for (int i = 0; i < numblobs; i++) {
+				int top = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
+						NIVision.MeasurementType.MT_BOUNDING_RECT_TOP);
+				int left = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
+						NIVision.MeasurementType.MT_BOUNDING_RECT_LEFT);
+				int width = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
+						NIVision.MeasurementType.MT_BOUNDING_RECT_WIDTH);
+				int height = (int) NIVision.imaqMeasureParticle(filteredframe, i, 0,
+						NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT);
+				blobs.add(new NIVision.Rect(top, left, height, width));
+			}
+			return blobs;
+		} else {
+			return null;
 		}
-		return blobs;
 	}
 }
